b948a2a61102a48c08c3586607bfb03f7ae174f2
임진택 <kococo9504@gmail.com>
2022-05-28 23:58:44 +0900

    #1 ADD SVG Size Hook

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,15 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import CandleStickChart from "./pages/CandleStickChart";
-import NotFound from "./pages/NotFound";
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import CandleStickChart from './pages/CandleStickChart'
+import NotFound from './pages/NotFound'
 export default function Router() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-        <Route path="candle" element={<CandleStickChart />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </HashRouter>
-  );
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<App />}></Route>
+                <Route path="/candle" element={<CandleStickChart />}></Route>
+                <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+        </HashRouter>
+    )
 }
--- src/pages/CandleStickChart.tsx
+++ src/pages/CandleStickChart.tsx
@@ -1,14 +1,21 @@
+import { useEffect, useRef } from 'react'
 import ChartContainer from '../components/ChartContainer'
 import Layout from '../components/Layout'
+import useChartSize from '../hooks/useChartSize'
 import useQuery from '../hooks/useQuery'
 const { VITE_DATA_BASE_URL } = import.meta.env
 export default function CandleStickChart() {
     const { data, status, errMsg } = useQuery(VITE_DATA_BASE_URL + 'amd_stock_price.json')
-
+    const svgRef = useRef<SVGSVGElement>(null)
+    const { width, height } = useChartSize(svgRef)
+    useEffect(() => {
+        console.log(width, height)
+    }, [width, height])
     return (
         <Layout>
             <ChartContainer title={'CandleStickChart'}>
-                <svg></svg>
+                {width}, {height}
+                <svg ref={svgRef} width="100%" height="100%"></svg>
             </ChartContainer>
         </Layout>
     )
