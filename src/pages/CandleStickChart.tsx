87edc653e59b24cb8e0898c15d1ac66f4a1d28b4
임진택 <kococo9504@gmail.com>
2022-05-28 15:57:17 +0900

    ADD Candlestick Data Fetcher

--- src/pages/CandleStickChart.tsx
+++ src/pages/CandleStickChart.tsx
@@ -1,9 +1,15 @@
-import Layout from "../components/Layout";
-
+import ChartContainer from '../components/ChartContainer'
+import Layout from '../components/Layout'
+import useQuery from '../hooks/useQuery'
+const { VITE_DATA_BASE_URL } = import.meta.env
 export default function CandleStickChart() {
-  return (
-    <Layout>
-      <span>CandleStickChart</span>
-    </Layout>
-  );
+    const { data, status, errMsg } = useQuery(VITE_DATA_BASE_URL + 'amd_stock_price.json')
+
+    return (
+        <Layout>
+            <ChartContainer title={'CandleStickChart'}>
+                <svg></svg>
+            </ChartContainer>
+        </Layout>
+    )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+
+export default ({ mode }) => {
+    // Load app-level env vars to node-level env vars.
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+    console.log(process.env)
+    return defineConfig({
+        plugins: [react()],
+        // To access env vars here use process.env.TEST_VAR
+    })
+}
