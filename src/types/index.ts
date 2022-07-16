62b8d8c3403cc92f584882433baca6c8106ef066
임진택 <kococo9504@gmail.com>
2022-07-16 16:16:32 +0900

    #1 Fix Constants File Name

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,19 @@
-export * from './DeafultConfig'
+export * from './DefaultConfig'
 
 export type LayoutType = {
     children: React.ReactNode
+}
+export type ChartDataType = {
+    dataset: any[]
+    xDomain: string[] | number[] | Date[]
+    yDomain: string[] | number[] | Date[]
+}
+export type YFinanceStockDatasetType = {
+    Date: string
+    Open: number
+    High: number
+    Low: number
+    Close: number
+    'Adj Close': number
+    Volume: number
 }
